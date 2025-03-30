60a3519992d7c128c9e6563ffec3fd6235bf9b86
Satish Gosu <gosusatti77@gmail.com>
2025-03-30 08:38:38 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -1,26 +1,31 @@
 import sys
 
-def error_message_detail(eror, error_detail: sys):
+def error_message_detail(error):
     """
-    This function takes an error and its details and returns a formatted string with the error message.
+    This function takes an error and returns a formatted string with its details.
     """
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    line_number = exc_tb.tb_lineno
+    exc_type, exc_value, exc_tb = sys.exc_info()  # Get exception details
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        file_name = "Unknown"
+        line_number = "Unknown"
+    
     error_message = f"Error occurred in script: [{file_name}] at line number: [{line_number}] error message: [{str(error)}]"
-    return error_message 
+    return error_message
 
 
 class CustomException(Exception):
     """
     Custom exception class that inherits from the built-in Exception class.
     """
-    def __init__(self, error_message, error_detail: sys):
+    def __init__(self, error_message):
         """
-        Initializes the CustomException with an error message and its details.
+        Initializes the CustomException with an error message.
         """
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = error_message_detail(error_message)
 
     def __str__(self):
         """
